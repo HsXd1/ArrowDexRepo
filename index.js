ac1bc9fbba62db4653deebe4d76b1eae82620c61
lasagnaman-a <hyperxdspeed@gmail.com>
2020-09-24 10:34:55 -0700

    added new prefix command

--- index.js
+++ index.js
@@ -1,77 +1,87 @@
 const Discord = require('discord.js');
-const botsettings = require('./botsettings.json');
 const bot = new Discord.Client({
-    disableEveryone: true
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 });
+const {
+    loadCommands
+} = require('./util/loadCommands')
+const mongoose = require('mongoose');
+const prefix = require('./models/prefix');
+mongoose.connect('mongodb+srv://hsxd:ninjago90@arrowdex.iegn3.mongodb.net/Data', { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+bot.login('NzEzNDYyODAxNjc4NTk4MTY0.XsgeAw.UyuHOTglDq8poXBOV4nnpg0H6kQ');
+
 const DBL = require("dblapi.js")
-const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcxMzQ2MjgwMTY3ODU5ODE2NCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTkyNTAxMDQ3fQ.6boC1m7UE0Pj0xP06tEghpSiCWPcisA4MotOr_Wg9Cw', bot)
+const dbl = new DBL ('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcxMzQ2MjgwMTY3ODU5ODE2NCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTkyNTAxMDQ3fQ.6boC1m7UE0Pj0xP06tEghpSiCWPcisA4MotOr_Wg9Cw', bot)
 const randomColor = Math.floor(Math.random() * 16777214) + 1
 
-require("./util/eventHandler")(bot)
-
-const fs = require("fs");
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
-fs.readdir("./commands/", (err, files) => {
-
-    if (err) console.log(err)
-
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
-    if (jsfile.length <= 0) {
-        return console.log("[LOGS] Couldn't Find Commands!");
-    }
-
-    jsfile.forEach((f, i) => {
-        let pull = require(`./commands/${f}`);
-        bot.commands.set(pull.config.name, pull);
-        pull.config.aliases.forEach(alias => {
-            bot.aliases.set(alias, pull.config.name)
-        });
-    });
-});
+loadCommands(bot);
 
 bot.on('ready', () => {
+    console.log(`${bot.user.username} is activated`)
+    bot.user.setActivity(`${bot.guilds.cache.size} servers! | s?help`, {type: "WATCHING"});
+    console.log(bot.guilds.cache.size)
     setInterval(() => {
         dbl.postStats(bot.guilds.size);
     }, 1800000);
 })
 bot.on("message", async message => {
+    if (message.author.bot) return;
 
-    if(message.author.bot || message.channel.type === "dm") return;
-     if (message.content === '<@!713462801678598164>') {
-         return message.channel.send('The prefix for Arrow Dex in this server is `s?`. (Example: `s?help`)')
-     }
-     if (message.content === 's?') {
-         return message.channel.send('The prefix for Arrow Dex in this server is `s?`. (Example: `s?help`)')
-     }
-    let prefix = botsettings.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = message.content.substring(message.content.indexOf(' ')+1);
+    //Getting the data from the model
+    const data = await prefix.findOne({
+        GuildID: message.guild.id
+    });
 
+    const messageArray = message.content.split(' ');
+    const cmd = messageArray[0];
+    const args = messageArray.slice(1);
 
+    //If there was a data, use the database prefix BUT if there is no data, use the default prefix which you have to set!
+    if(data) {
+        const prefix = data.Prefix;
 
-    if(!message.content.startsWith(prefix)) return;
-    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
-    if(commandfile) commandfile.run(bot,message,args)
+        if (!message.content.startsWith(prefix)) return;
+        const commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
+        commandfile.run(bot, message, args);
+    } else if (!data) {
+        //set the default prefix here
+        const prefix = "s?";
 
+        if (!message.content.startsWith(prefix)) return;
+        const commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
+        commandfile.run(bot, message, args);
+
+    }
+    if (message.content === '<@!713462801678598164>') {
+        return message.channel.send('The prefix for Arrow Dex in this server is `s?`. (Example: `s?help`)')
+    }
+    if (message.content === 's?') {
+        return message.channel.send('The prefix for Arrow Dex in this server is `s?`. (Example: `s?help`)')
+    }
+   
     if (message.content.startsWith(prefix)) {
-        const logchannel = bot.channels.cache.get('751690728156954654');
-        const logChannelEmbed = new Discord.MessageEmbed()
-        .setTitle('Command Used!')
-        .setColor(randomColor)
-        .addFields({
-          name: 'User:', value: `\`${message.author.tag}\``
-        }, {
-          name: 'Command Used:', value: `\`${message.content}\``
-        }, {
-          name: 'Server:', value: `\`${message.guild.name}\``
-        })
-        .setTimestamp()
-        logchannel.send(logChannelEmbed);
-        console.log(`${message.author.username}\` used \`${message.content}\` in \`${message.guild.name}`);
-     }
-
+       const logchannel = bot.channels.cache.get('751690728156954654');
+       const logChannelEmbed = new Discord.MessageEmbed()
+       .setTitle('Command Used!')
+       .setColor(randomColor)
+       .addFields({
+         name: 'User:', value: `\`${message.author.tag}\``
+       }, {
+         name: 'Command Used:', value: `\`${message.content}\``
+       }, {
+         name: 'Server:', value: `\`${message.guild.name}\``
+       })
+       .setTimestamp()
+       logchannel.send(logChannelEmbed);
+       console.log(`${message.author.username}\` used \`${message.content}\` in \`${message.guild.name}`);
+    }
 })
-bot.login(process.env.token);
+
+
